4014d74960f8dc9b3cba5415bd85198b6175308d
Micron-Flash <redman2118@gmail.com>
2019-07-05 18:09:46 -0600

    Homing 1 motor (working)

--- GetPoints/MotorMovmentTest/MotorMovmentTest.ino
+++ GetPoints/MotorMovmentTest/MotorMovmentTest.ino
@@ -15,25 +15,25 @@
   #define MOTOR_RPM 60 //starting rpm
   #define MOTOR_ACCEL 200
   #define MOTOR_DECEL 100
-  #define DIR_1 4 // Pins for motor 1
-  #define STEP_1 5 //
+  #define DIR_1 2 // Pins for motor 1
+  #define STEP_1 3 //
   #define HOMING1 7 // Left endstop
   #define HOMING2 8 // Right endstop
-  #define DIR_2 2 // Pins for motor 2
-  #define STEP_2 3
+  #define DIR_2 4 // Pins for motor 2
+  #define STEP_2 5
   
   #define MICROSTEPS 16 // Microstepping 
   /*
-  200  full steps * 16 = 3200 steps per rotation + 1:4 gear ratio
+   200  full steps * 16 = 3200 steps per rotation + 1:4 gear ratio
    2.51307087 in per rotation
    12,800 steps per rotation
   */
   BasicStepperDriver stepper1(MOTOR_STEPS, DIR_1, STEP_1);
   BasicStepperDriver stepper2(MOTOR_STEPS, DIR_2, STEP_2);
 
- Servo servo1;
+  Servo servo1;
 
- int servoPos = 100;
+  int servoPos = 100;
  
   MultiDriver controller(stepper1, stepper2);
 
@@ -84,7 +84,7 @@
   bool triggered1 = false;
   bool triggered2 = false;
  
-  float testX[] = {};
+  float testX[] = {}; 
   float testY[] = {};
   
   
@@ -94,168 +94,97 @@ void setup() {
   pinMode(HOMING2, INPUT);
   Serial.begin(9600);
   Serial.print("Initializing SD card...");
-   pinMode(10, OUTPUT);
- 
+  pinMode(10, OUTPUT);
   if (!SD.begin(10)) {
     Serial.println("initialization failed!");
     return;
   }
   Serial.println("initialization done.");
- 
-  
 
-  
   stepper1.begin(MOTOR_RPM, MICROSTEPS);
   stepper2.begin(MOTOR_RPM, MICROSTEPS);
   servo1.attach(6);
-  
-  //stepper1.setSpeedProfile(stepper1.LINEAR_SPEED, MOTOR_ACCEL, MOTOR_DECEL);
- // stepper2.setSpeedProfile(stepper2.LINEAR_SPEED, MOTOR_ACCEL, MOTOR_DECEL);
-   
- // AC = sqrt(sq((aX-cX)) + sq((aY-cY))); //1211.012
- // BC = sqrt(sq((bX-cX)) + sq((bY-cY)));
-// AC = 31;
-//BC = 32.5;
 
   cY = (sq(AB)+sq(ACMax) - sq(BCMax))/(2 *AB);
   cX = sqrt(sq(ACMax - sq(cY)));
 }
 
 void loop() {
-  
-// Serial.println(digitalRead(HOMING1));
-if (homedFinal == false){  
-      if(homed1 == false){
-          stepper1.startMove(round(3/0.0049087421875)); // move enough for it to triger at least once
-          homed1 = true;
-        }
-     if(homed2 == false){
-          stepper2.startMove(round(3/0.0049087421875)); // move enough for it to triger at least once
-          homed2 = true;
-        }
-      if (digitalRead(HOMING1) == HIGH){
-          triggered1 = true;
-      
-      }
-      if (digitalRead(HOMING2) == HIGH){
-          triggered2 = true;
-      }
-      
-      if (stepper1.nextAction() <= 0){
-        if (triggered1){
-          homed1 = false;
-          //Serial.println("Still Goint=");
-          triggered1 = false;
-        }
-        else{
-          homed1 = true;
-          AC = ACMax;
-        }
-      }
-       if (stepper2.nextAction() <= 0){
-        if (triggered2){
-          homed2 = false;
-          
-          triggered2 = false;
-        }
-        else{
-          homed2 = true;
-          BC = BCMax;
-        }
-      }
-      if (homed1 && homed2 == true){
+  if (homedFinal == false){  
+     if ((triggered1 == true) && (stepper1.nextAction() <= 0)){
+      stepper1.startMove (round(4/0.0049087421875));
+       triggered1 = false;
+     }   
+   if (digitalRead(HOMING1) == HIGH){
+     triggered1 = true;  
+   }
+   if (stepper1.nextAction() <= 0){
+      if(triggered1 == false){
+        AC = ACMax;
         homedFinal = true;
       }
+    }
   }
-   
-
-
-     if(homedFinal){   
-         File posFile = SD.open ("test.txt", FILE_READ);
-      if(posFile){
-        
-          char fileContents[9]; // Probably can be smaller
+  
+   if(homedFinal){   
+       File posFile = SD.open ("test.txt", FILE_READ); // !!!Change the file name on this to what ever you will put on the SD card!!!!
+     if(posFile){
+       char fileContents[9]; // Probably can be smaller
        byte index = 0;
-       while (posFile.available()) 
-      {
-          char aChar = posFile.read();
-          if(aChar != '\n' && aChar != '\r')
-          {
-             fileContents[index++] = aChar;
-             fileContents[index] = '\0'; // NULL terminate the array
-          }
-          else // the character is CR or LF
-          {
-            // Serial.print("fileContents: [");
-           //  Serial.print(fileContents);
-           //  Serial.println("]");
-             if(strlen(fileContents) > 0)
-             {
-              if (rocker == 0){
-                X = atof(fileContents);
-                rocker = 1;
-              // Serial.print("X: ");
-              //   Serial.println(X);
-              }
-              else {
-                Y = atof(fileContents);
-                rocker = 0;
-              //  Serial.print("Y: ");
-             //    Serial.println(Y);
-              }
-                //float aVal = atof(fileContents);
-    
-                if (rocker == 0 ){
-                  targetCx = X;
-                  targetCy = Y;
-                   newAC = sqrt(sq((aX-targetCx)) + sq((aY-targetCy)));
-                   newBC = sqrt(sq((bX-targetCx)) + sq((bY-targetCy)));
-    
-                   changeAC = newAC - AC;// 
-                   changeBC = BC - newBC;
-                   Serial.println(changeAC);
-                   if (sqrt(sq(changeAC)) >= 5 || sqrt(sq(changeBC)) >= 5){
-                    if (drawing == true){
-                      Serial.println("LOL");
-                     for (servoPos = 140; servoPos >= 110; servoPos -= 1) { // goes from 180 degrees to 0 degrees
-                     servo1.write(servoPos);              // tell servo to go to position in variable 'pos'
-                     delay(15);                       // waits 15ms for the servo to reach the position
+       while (posFile.available()){
+         char aChar = posFile.read();
+         if(aChar != '\n' && aChar != '\r'){
+           fileContents[index++] = aChar;
+           fileContents[index] = '\0';
+         }
+         else{
+           if(strlen(fileContents) > 0){
+             if (rocker == 0){ // we will use the rocker to set the first 5 or 6 numbers on the fille to set machine settings
+               X = atof(fileContents);
+               rocker = 1;
+             }
+             else {
+               Y = atof(fileContents);
+               rocker = 0;
+             }
+             if (rocker == 0 ){
+                targetCx = X;
+                targetCy = Y;
+                newAC = sqrt(sq((aX-targetCx)) + sq((aY-targetCy))); // we find the new length of the chain
+                newBC = sqrt(sq((bX-targetCx)) + sq((bY-targetCy)));
+                changeAC = newAC - AC; // we figure out the difference between the actual and the target length
+                changeBC = BC - newBC; 
+                if (sqrt(sq(changeAC)) >= 5 || sqrt(sq(changeBC)) >= 5){ // We check for a large movement. This usually idicates when the pen should be lifted. (Seems to be working well)
+                  if (drawing == true){
+                    for (servoPos = 140; servoPos >= 110; servoPos -= 1) {
+                      servo1.write(servoPos);             
+                      delay(15);                      
+                    }
+                  }
+                   drawing = false;                 
+                  }
+                  else{
+                    if (drawing == false){
+                      for (servoPos = 110; servoPos <= 140; servoPos += 1) { 
+                        servo1.write(servoPos);              
+                        delay(15);                       
                       }
                     }
-                     drawing = false;                 
-                   }
-                  else{
-                     if (drawing == false){
-                      for (servoPos = 110; servoPos <= 140; servoPos += 1) { // goes from 0 degrees to 180 degrees
-                     // in steps of 1 degree
-                     servo1.write(servoPos);              // tell servo to go to position in variable 'pos'
-                      delay(15);                       // waits 15ms for the servo to reach the position
-                            }
-                     
-                     
-                     }
                     drawing = true;  
                   }
-    
-                   moveACsteps = round(changeAC/0.0049087421875)*scale;
-                   moveBCsteps = round(changeBC/0.0049087421875)*scale;
-                  //Serial.println(moveBCsteps);
-                  
+                  moveACsteps = round(changeAC/0.0049087421875)*scale; // we move the amount of stepps it will take to move the chain a certain amount of mm. The long number is the conversion of steps to mm (example: to move 1 mm it would take 203.718 steps)
+                  moveBCsteps = round(changeBC/0.0049087421875)*scale;
                   controller.move(moveACsteps,moveBCsteps);
-                  
                   AC = newAC;
                   BC = newBC;
-                }
              }
-    
-             fileContents[0] = '\0';
-             index = 0;
-          }
+           }
+           fileContents[0] = '\0';
+           index = 0;
+         }
        }
-  
      }
-  
-  }
+   }
 }
 
 
